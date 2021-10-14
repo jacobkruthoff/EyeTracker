12c1850d902ce15cb47e31fb2971075499d9e181
jacobkruthoff <52637752+jacobkruthoff@users.noreply.github.com>
2021-10-14 16:23:12 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-(() => {
+function testFunc() {
   let button = document.createElement("button");
   document.append(button);
 
   console.log("hello chicken nuggets");
-})();
+}
+
+function trying() {
+  window.alert("message");
+}
