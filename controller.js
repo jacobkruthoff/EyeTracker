22f37ea58c1a02aad4bec7fe0f704b003be3d4b3
Jacob Kruthoff <jacob@kruthoffs.com>
2021-11-03 16:46:38 -0700

    wip

--- controller.js
+++ controller.js
@@ -2,6 +2,16 @@ window.saveDataAcrossSessions = true
 // array to store webgazer data
 // init with headers
 let dataArr = [["X", "Y"]];
+let csv1;
+let csv2;
+let csv3;
+let pics = 3;
+
+for(let i = 0; i < pics; i++){
+  setTimeout(run, 30000);
+}
+
+
 
 //while pictures are present
 //callback gaze listener
@@ -11,20 +21,34 @@ let dataArr = [["X", "Y"]];
 //on dom content loaded, start timer
 // after 30 seconds, pause data collection
 
-webgazer.setGazeListener((data) => {
+function run() {
 
-  if(data == null){
-    return;
-  }
-  //push x and y coordinates to array
-  dataArr.push([data.x, data.y]);
-  //format to csv and store in csvContent variable for export
-  let csvContent = "data:text/csv;charset=utf-8,"
-    + dataArr.map(e => e.join(",")).join("\n");
+  webGazeListen();
+  webgazer.pause();
+  //export data
+  //do array here!
   
-  //replace console.log with SMTP
-  console.log(csvContent);
+  
+}
 
-}).begin()
+function webGazeListen() {
+  webgazer.setGazeListener((data) => {
+
+    if (data == null) {
+      return;
+    }
+    //push x and y coordinates to array
+    dataArr.push([data.x, data.y]);
+    //format to csv and store in csvContent variable for export
+    let csvContent = "data:text/csv;charset=utf-8,"
+      + dataArr.map(e => e.join(",")).join("\n");
+
+    //replace console.log with SMTP
+    console.log(csvContent);
+
+  }).begin()
+}
+
+
 
 webgazer.pause();
