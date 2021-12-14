5a6d891bb1793fd51c2da76bf4d9a6f6816426c8
Jacob Kruthoff <jacob@kruthoffs.com>
2021-12-14 15:56:41 -0800

    cleanup

--- controller.js
+++ controller.js
@@ -7,7 +7,6 @@ let csv2;
 let csv3;
 let counter = 0;
 // array to store csv files
-//let csvArr = [csv1, csv2, csv3];
 let csvArr = [];
 //change depending on the amount of images
 let pics = 3;
@@ -19,19 +18,8 @@ let time = 29500;
 function pausing() {
   console.log("pausing");
   webgazer.pause();
-  //export data
-  //do array here!
   console.log("after run")
   csvArr.push(csv1, csv2);
-
-  
-
-  console.log(csvArr.length);
-  console.log(csv1.length);
-  console.log(csv2.length);
-  //console.log(csv3.length);
-  // console.log(csvArr.length);
-  // console.log(csvArr[0]);
 
 }
 
@@ -50,38 +38,6 @@ function exportData(){
   }
 }
 
-// function sendData(){
-//   var nodemailer = require('nodemailer');
-//   let transport = nodemailer.createTransport({
-//     service: "Gmail",
-//     port: 465,
-//     secure: true,
-//     auth:{
-//       user: "plnueyetracker@gmail.com",
-//       pass: "webgazerintegration",
-//     }
-//   });
-
-//   let email = {
-//     from: "plnueyetracker@gmail.com",
-//     to: "yacomctaco@gmail.com",
-//     subject: "test",
-//     text: "did this work",
-//     html: "<b>HELLLLOOOOO</b>",
-//   };
-
-//   transport.sendMail(email, function(error, info){
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log("email sent " + info.response);
-//     }
-//   });
-
-//   console.log("message sent: %s", email.messageId)
-// }
-// sendData();
-
 function run() {
   //this is just webgazer running, not storing data in CSV format
   //Used for calibrting Webgazer
@@ -94,23 +50,14 @@ function run() {
   setTimeout(function () {
     console.log("Default");
     webGazeListen();
-    // counter = counter + 1;
-    // console.log("data 1");
-    // console.log(csvArr[0]);
-
+  
     setTimeout(function () {
       console.log("Image 1");
       webGazeListen();
-      counter = counter + 1;
-      console.log("data 2");
-      //console.log(csvContent[1]);
 
       setTimeout(function () {
         console.log("Image 2");
         webGazeListen();
-        counter = counter + 1;
-        console.log("data 3");
-        //console.log(csvContent[2]);
 
         setTimeout(function () {  //stops running after 3 images. If you wish to add more images, copy pattern above
           pausing();
@@ -130,36 +77,20 @@ function webGazeListen() {
     //push x and y coordinates to array
     dataArr.push([data.x, data.y]);
 
-    console.log("counter at " +counter);
-
     switch (counter) {
       case 0:
         csv1 = "data:text/csv;charset=utf-8,"
           + dataArr.map(e => e.join(",")).join("\n");
-          console.log("csv1 undefined " + (csv1 == undefined));
-        //csvArr.push(csv1);
         break;
       case 1:
         csv2 = "data:text/csv;charset=utf-8,"
           + dataArr.map(e => e.join(",")).join("\n");
-          console.log("csv2 undefined " + (csv2 == undefined));
-        //csvArr.push(csv2);
         break;
       case 2:
         csv3 = "data:text/csv;charset=utf-8,"
           + dataArr.map(e => e.join(",")).join("\n");
-          console.log("csv3 undefined " + (csv3 == undefined));
-        //csvArr.push(csv3);
         break;
     }
-    //format to csv and store in csvContent variable for export
-    // let csvContent = "data:text/csv;charset=utf-8,"
-    //   + dataArr.map(e => e.join(",")).join("\n");
-
-    //replace console.log with SMTP
-    //console.log(csvContent);
-
-
   }).begin()
 
 }
@@ -168,12 +99,3 @@ function webGazeListen() {
 //Start tracking
 console.log("STARTING");
 run();
-
-
-//while pictures are present
-//callback gaze listener
-//pause listener on image transitions
-
-//wrap listener in timeout function
-//on dom content loaded, start timer
-// after 30 seconds, pause data collection
